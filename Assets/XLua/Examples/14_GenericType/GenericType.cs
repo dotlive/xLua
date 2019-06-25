cd4a8a5d5062b907be1bd2145be03373dedfe9b7
dotlive <dotlive@163.com>
2019-06-26 07:01:59 +0800

    add genericType test;

---
+++ Assets/XLua/Examples/14_GenericType/GenericType.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+public class GenericType
+{
+    public static List<int> GetList()
+    {
+        List<int> ret = new List<int>();
+        ret.Add(1);
+        ret.Add(2);
+        ret.Add(3);
+        return ret;
+    }
+}
+
